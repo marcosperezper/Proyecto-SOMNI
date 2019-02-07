33d55b9f6f4be2c7967527006f42de06a84ab8e9
alejandroartess <alarso@hotmail.com>
2019-02-07 09:54:08 +0100

    Adding new routes

---
+++ CF_Veterans/src/AppBundle/Controller/RootController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RootController extends Controller
+{
+    /**
+     * @Route("/index", name="index")
+     */
+    public function showIndex(Request $request)
+    {
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/lineup", name="lineup")
+     */
+    public function showLineup(Request $request)
+    {
+        return $this->render('lineup.html.twig');
+    }
+
+    /**
+     * @Route("/media", name="media")
+     */
+    public function showMedia(Request $request)
+    {
+        return $this->render('media.html.twig');
+    }
+
+    /**
+     * @Route("/news", name="news")
+     */
+    public function showNews(Request $request)
+    {
+        return $this->render('news.html.twig');
+    }
+
+    /**
+     * @Route("/roster", name="roster")
+     */
+    public function showRoster(Request $request)
+    {
+        return $this->render('roster.html.twig');
+    }
+
+    /**
+     * @Route("/stats", name="stats")
+     */
+    public function showStats(Request $request)
+    {
+        return $this->render('stats.html.twig');
+    }
+}
